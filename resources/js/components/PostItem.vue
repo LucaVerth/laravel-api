10a436d01826b90016c0f9c0c31df54d6de319a5
LucaVerth <luca.verthuy@gmail.com>
2022-02-09 18:41:01 +0100

    Added date

--- resources/js/components/PostItem.vue
+++ resources/js/components/PostItem.vue
@@ -4,22 +4,34 @@
             img Placeholder
         </div>
         <div class="post-content">
-          <a href="#"><h3>{{ post.title}}</h3></a>
+          <a href="#"><h3>{{ post.title }}</h3></a>
           <p class="main-content-post">
-            {{post.content}}
+            {{ extractText }}
           </p>
         </div>
         <div class="post-details">
-            {{post.created_at}}
+            {{ getDate }}
         </div>
     </div>
 </template>
 
 <script>
+
+import dayjs from "dayjs";
+
 export default {
     name: 'PostItem',
     props:{
         'post': Object
+    },
+    computed: {
+        extractText(){
+            return this.post.content.substr(0, 60) + '...';
+        },
+        getDate(){
+            const date = new Date(this.post.created_at);
+            return dayjs(date).format("YYYY-MM-DD");
+        }
     },
 }
 </script>
